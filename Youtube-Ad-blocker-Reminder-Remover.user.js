3e7b0cf8d9c1d75115d40073f58e5c5141548579
Laurie <laurie201@protonmail.ch>
2023-10-19 14:41:20 +0100

    Block display ads

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -117,6 +117,7 @@
             const skipBtn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button');
             const ad = [...document.querySelectorAll('.ad-showing')][0];
             const sidAd = document.querySelector('ytd-action-companion-ad-renderer');
+            const displayAd = document.querySelector('div#root.style-scope.ytd-display-ad-renderer.yt-simple-endpoint');
             if (ad)
             {
                 document.querySelector('video').playbackRate = 10;
@@ -129,6 +130,9 @@
             if (sidAd)
             {
                 sidAd.remove();
+            }
+            if (displayAd) {
+                displayAd.remove();
             }
         }, 50)
     }
