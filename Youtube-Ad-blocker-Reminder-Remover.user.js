c31b817a0a95b0d3ec376f10f536f67dfb56a412
Joelmatic <78729990+TheRealJoelmatic@users.noreply.github.com>
2023-11-02 18:03:15 +0000

    Update Youtube-Ad-blocker-Reminder-Remover.user.js

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Adblock Thing
 // @namespace    http://tampermonkey.net/
-// @version      2.0
+// @version      2.1
 // @description  Removes Adblock Thing
 // @author       JoelMatic
 // @match        https://www.youtube.com/*
