ea6ca6d22abaf93ed858a98ae687c54a7f381c31
A Dev <sof26110@gmail.com>
2024-01-20 19:58:36 +0100

    Update Youtube-Ad-blocker-Reminder-Remover.user.js

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -284,7 +284,7 @@
 
     function checkForUpdate(){
 
-        if (!(window.location.href.includes("youtube.com"))){
+        if (window.top !== window.self && !(window.location.href.includes("youtube.com"))){
             return;
         }
 
