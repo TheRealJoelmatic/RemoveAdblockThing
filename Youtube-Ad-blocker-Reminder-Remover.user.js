1d57f02ab2d1ed124f8497b2973e9cb256062de7
LordElias <55048695+LordEliasTM@users.noreply.github.com>
2024-01-21 12:44:32 +0100

    Update Youtube-Ad-blocker-Reminder-Remover.user.js

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -9,6 +9,7 @@
 // @updateURL    https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
 // @downloadURL  https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
 // @grant        none
+// @require      https://raw.githubusercontent.com/pladaria/requestidlecallback-polyfill/master/index.js
 // ==/UserScript==
 
 (function()
@@ -79,7 +80,8 @@
 
     // Remove Them pesski popups
     function popupRemover() {
-        setInterval(() => {
+        const removePopupLoop = () => {
+            requestIdleCallback(removePopupLoop, {timeout: 500});
             const modalOverlay = document.querySelector("tp-yt-iron-overlay-backdrop");
             const popup = document.querySelector(".style-scope ytd-enforcement-message-view-model");
             const popupButton = document.getElementById("dismiss-button");
@@ -109,7 +111,8 @@
                 log("Popup removed");
             }
 
-        }, 1000);
+        }
+        requestIdleCallback(removePopupLoop);
     }
     // undetected adblocker method
     function removeAds()
@@ -118,7 +121,8 @@
 
         var videoPlayback = 1;
 
-        setInterval(() =>{
+        const removeAdsFunc = () => {
+            requestIdleCallback(adblockerFunc, {timeout: 50});
 
             var video = document.querySelector('video');
             const ad = [...document.querySelectorAll('.ad-showing')][0];
@@ -222,7 +226,8 @@
                 }
             }
 
-        }, 50)
+        }
+        requestIdleCallback(removeAdsFunc);
 
         removePageAds();
     }
