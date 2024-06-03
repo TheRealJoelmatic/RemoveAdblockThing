9fb292bbfa4da23db1edb4d99eaad4e389c6a9d6
Nexa-Search <166246365+Open-Horizon-Games@users.noreply.github.com>
2024-06-03 06:22:26 +0100

    Update Youtube-Ad-blocker-Reminder-Remover.user.js

--- Youtube-Ad-blocker-Reminder-Remover.user.js
+++ Youtube-Ad-blocker-Reminder-Remover.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
-// @name         Lightweight YouTube Ad Blocker
+// @name         Enhanced Remove Adblock Thing
 // @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  Blocks YouTube ads by removing ad scripts and elements
+// @version      1.0
+// @description  Removes Adblock Thing and improves YouTube experience
 // @author       JoelMatic
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @updateURL    https://github.com/Open-Horizon-Games/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
+// @downloadURL  https://github.com/Open-Horizon-Games/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
 // @grant        none
 // ==/UserScript==
 
